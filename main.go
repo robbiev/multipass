e6ed729dce0f8393ea76a4bf7851c7ba063cd682
Robbie Vanbrabant <robbie.vanbrabant@gmail.com>
2014-05-03 17:31:56 +0100

    gofmt, clean up debug printlns

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"encoding/json"
+	"bytes"
+	"code.google.com/p/go.crypto/pbkdf2"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/sha1"
 	"encoding/base64"
-  "io/ioutil"
+	"encoding/json"
 	"fmt"
-  "os"
-  "bytes"
 	"github.com/howeyc/gopass"
-  "code.google.com/p/go.crypto/pbkdf2"
-  "crypto/sha1"
-  "crypto/md5"
-  "crypto/aes"
-  "crypto/cipher"
+	"io/ioutil"
+	"os"
 )
 
 func clear(b []byte) {
-  for i := 0; i < len(b); i++ {
-    b[i] = 0;
-  }
+	for i := 0; i < len(b); i++ {
+		b[i] = 0
+	}
 }
 
 // Pad applies the PKCS #7 padding scheme on the buffer.
@@ -37,24 +37,20 @@ func Pad(in []byte) []byte {
 // invalid, nil is returned.
 func Unpad(in []byte) []byte {
 	if len(in) == 0 {
-    fmt.Println("x1")
 		return nil
 	}
 
 	padding := in[len(in)-1]
-  fmt.Printf("len %s\n", len(in))
-  fmt.Printf("padding %s\n", padding)
+	fmt.Printf("len %s\n", len(in))
+	fmt.Printf("padding %s\n", padding)
 	if int(padding) > len(in) || padding > aes.BlockSize {
-    fmt.Println("x2")
 		return nil
 	} else if padding == 0 {
-    fmt.Println("x3")
 		return nil
 	}
 
 	for i := len(in) - 1; i > len(in)-int(padding)-1; i-- {
 		if in[i] != padding {
-    fmt.Println("x4")
 			return nil
 		}
 	}
@@ -64,142 +60,87 @@ func Unpad(in []byte) []byte {
 func main() {
 	fmt.Printf("Password: ")
 	pass := gopass.GetPasswd()
-  defer clear(pass)
+	defer clear(pass)
 
-  type PassKey struct {
-    Data string
-    Identifier string
-    Iterations int
-    Level string
-    Validation string
-  }
-  type Keys struct {
-    List []PassKey
-  }
-  file, err := ioutil.ReadFile("./encryptionKeys.js")
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	type PassKey struct {
+		Data       string
+		Identifier string
+		Iterations int
+		Level      string
+		Validation string
+	}
+	type Keys struct {
+		List []PassKey
+	}
+	file, err := ioutil.ReadFile("./encryptionKeys.js")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-  var keys Keys
-  err = json.Unmarshal(file, &keys)
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	var keys Keys
+	err = json.Unmarshal(file, &keys)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-  fmt.Printf("%+v\n", keys)
+	fmt.Printf("%+v\n", keys)
 
-  data := keys.List[1].Data
+	data := keys.List[1].Data
 
-  // seems to be a null terminated string, cut off the last character
-  encryptionKey, er := base64.StdEncoding.DecodeString(data[0:len(data)-1])
+	// seems to be a null terminated string, cut off the last character
+	encryptionKey, er := base64.StdEncoding.DecodeString(data[0 : len(data)-1])
 
-  if er != nil {
-    fmt.Println(er)
-    os.Exit(1)
-  }
+	if er != nil {
+		fmt.Println(er)
+		os.Exit(1)
+	}
 
-  saltMarker := []byte("Salted__")
-  salted := (bytes.Equal([]byte(encryptionKey[0:len(saltMarker)]), saltMarker))
-  //fmt.Println(encryptionKey[0:30])
-  //fmt.Println(len(encryptionKey))
+	saltMarker := []byte("Salted__")
+	salted := (bytes.Equal([]byte(encryptionKey[0:len(saltMarker)]), saltMarker))
 
-  var salt []byte
-  if salted {
-    // salt marker (8 bytes) | salt (8 bytes) | data
-    skip := len(saltMarker) + 8
-    salt = []byte(encryptionKey[len(saltMarker):skip])
-    //encryptionKey = encryptionKey[skip:len(encryptionKey)-skip]
-    encryptionKey = encryptionKey[skip:]
-  }
+	var salt []byte
+	if salted {
+		// salt marker (8 bytes) | salt (8 bytes) | data
+		skip := len(saltMarker) + 8
+		salt = []byte(encryptionKey[len(saltMarker):skip])
+		encryptionKey = encryptionKey[skip:]
+	}
 
-  //fmt.Println(encryptionKey[0:30])
-  //fmt.Println(len(encryptionKey))
-  //fmt.Println(len(salt))
+	// first 16 bytes are the key, last 16 bytes are the IV
+	bytez := pbkdf2.Key(pass, salt, keys.List[1].Iterations, aes.BlockSize*2, sha1.New)
+	key := bytez[:16]
+	iv := bytez[16:]
 
-  // first 16 bytes are the key, last 16 bytes are the IV
-  bytez := pbkdf2.Key(pass, salt, keys.List[1].Iterations, aes.BlockSize * 2, sha1.New)
-  key := bytez[:16]
-  iv := bytez[16:]
+	b, e := Decryptr(key, iv, encryptionKey)
+	if !e {
+		fmt.Println("decrypt error")
+		os.Exit(1)
+	}
 
-  b, e := Decryptr(key, iv, encryptionKey)
-  //b, e := Decrypt(bytez, encryptionKey)
-  if !e {
-    fmt.Println("decrypt error")
-    os.Exit(1)
-  }
+	fmt.Println(len(b))
+	fmt.Println(b)
 
-  fmt.Println(len(b))
-  fmt.Println(b)
+	validationData := keys.List[1].Validation
+	validation, er := base64.StdEncoding.DecodeString(validationData[0 : len(validationData)-1])
+	v := decryptData(b, validation)
+	fmt.Println(len(v))
+	fmt.Println(v)
 
-  validationData := keys.List[1].Validation
-  validation, er := base64.StdEncoding.DecodeString(validationData[0:len(validationData)-1])
-  //fmt.Println(validation[:8])
-  v := decryptData(b, validation)
-  fmt.Println(len(v))
-  fmt.Println(v)
+	fmt.Println(bytes.Equal(b, v))
 
-  fmt.Println(bytes.Equal(b, v))
-
-  fmt.Println("done")
-}
-
-func Decrypt2(k, in []byte) ([]byte, bool) {
-  if len(in) == 0 || len(in)%aes.BlockSize != 0 {
-    fmt.Println("meh")
-    return nil, false
-  }
-
-  c, err := aes.NewCipher(k)
-  if err != nil {
-    fmt.Println("meh2")
-    return nil, false
-  }
-
-  cbc := cipher.NewCBCDecrypter(c, in[:aes.BlockSize])
-  cbc.CryptBlocks(in[aes.BlockSize:], in[aes.BlockSize:])
-  out := in[aes.BlockSize:]
-  if out == nil {
-    fmt.Println("meh3")
-    return nil, false
-  }
-  return out, true
-
-}
-func Decrypt(k, in []byte) ([]byte, bool) {
-  if len(in) == 0 || len(in)%aes.BlockSize != 0 {
-    fmt.Println("meh")
-    return nil, false
-  }
-
-  c, err := aes.NewCipher(k)
-  if err != nil {
-    fmt.Println("meh2")
-    return nil, false
-  }
-
-  cbc := cipher.NewCBCDecrypter(c, in[:aes.BlockSize])
-  cbc.CryptBlocks(in[aes.BlockSize:], in[aes.BlockSize:])
-  out := in[aes.BlockSize:]
-  if out == nil {
-    fmt.Println("meh3")
-    return nil, false
-  }
-  return out, true
-
+	fmt.Println("done")
 }
 
 func decryptData(key, data []byte) []byte {
-  salt := data[8:16]
-  data = data[16:]
-  nkey := md5.Sum(append(key, salt...))
-  iv := md5.Sum(append(append(nkey[:], key...), salt...))
+	salt := data[8:16]
+	data = data[16:]
+	nkey := md5.Sum(append(key, salt...))
+	iv := md5.Sum(append(append(nkey[:], key...), salt...))
 
-  //result, _ := Decrypt(append(nkey[:], iv[:]...), data)
-  result, _ := Decryptr(nkey[:], iv[:], data)
-  return result
+	result, _ := Decryptr(nkey[:], iv[:], data)
+	return result
 }
 
 // https://leanpub.com/gocrypto/read#leanpub-auto-encrypting-and-decrypting-data-with-aes-cbc
@@ -208,17 +149,13 @@ func decryptData(key, data []byte) []byte {
 func Decryptr(k, iv, in []byte) ([]byte, bool) {
 	c, err := aes.NewCipher(k)
 	if err != nil {
-    fmt.Println("meh2")
 		return nil, false
 	}
 
 	cbc := cipher.NewCBCDecrypter(c, iv)
 	cbc.CryptBlocks(in, in)
-  fmt.Println("decrypted")
-  fmt.Println(len(in))
 	out := Unpad(in)
 	if out == nil {
-    fmt.Println("meh3")
 		return nil, false
 	}
 	return out, true
